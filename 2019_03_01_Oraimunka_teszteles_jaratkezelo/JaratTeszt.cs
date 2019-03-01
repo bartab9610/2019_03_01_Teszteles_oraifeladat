ed9f1ddebc40eef175c3bf4d9dd321e843960854
bartab9610 <barnabas96@freemail.hu>
2019-03-01 13:14:09 +0100

    Egyedi járatszám

--- 2019_03_01_Oraimunka_teszteles_jaratkezelo/JaratTeszt.cs
+++ 2019_03_01_Oraimunka_teszteles_jaratkezelo/JaratTeszt.cs
@@ -10,6 +10,21 @@ namespace _2019_03_01_Oraimunka_teszteles_jaratkezelo
     [TestFixture]
     class JaratTeszt
     {
+        Jaratkezelo Jarat;
+        [SetUp]
+        public void SetUp()
+        {
+            Jarat = new Jaratkezelo();
+        }
 
+        [Test]
+        public void Jarat_ugyanaz()
+        {
+            Jarat.UjJarat("Debreczeni Laci", "suli", "kocsma", DateTime.Now);
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Jarat.UjJarat("Debreczeni Laci", "suli", "kocsma", DateTime.Now);
+            });
+        }
     }
 }
--- 2019_03_01_Oraimunka_teszteles_jaratkezelo/Jaratkezelo.cs
+++ 2019_03_01_Oraimunka_teszteles_jaratkezelo/Jaratkezelo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2019_03_01_Oraimunka_teszteles_jaratkezelo
 {
-    class Class1
+    class Jaratkezelo
     {
         class Jarat
         {
@@ -16,7 +16,7 @@ namespace _2019_03_01_Oraimunka_teszteles_jaratkezelo
             public DateTime Indulas { get; set; }
             public int Keses { get; set; }
 
-            public Jarat(string jaratszam, string honnanRepter, string hovaRepter, DateTime indulas, int keses)
+            public Jarat(string jaratszam, string honnanRepter, string hovaRepter, DateTime indulas)
             {
                 this.Jaratszam = jaratszam;
                 this.HonnanRepter = honnanRepter;
@@ -25,19 +25,28 @@ namespace _2019_03_01_Oraimunka_teszteles_jaratkezelo
                 this.Keses = 0;
             }
         }
-        void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        List<Jarat> Jaratok_lista = new List<Jarat>();
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            var jarat = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            for (int i = 0; i < Jaratok_lista.Count; i++)
+            {
+                if (Jaratok_lista[i].Jaratszam.Equals(jaratSzam))
+                {
+                    throw new ArgumentException("A járatszám ugyanaz!");
+                }
+            }
+            Jaratok_lista.Add(jarat);
+        }
+        public void Keses(string jaratSzam, int keses)
         {
 
         }
-        void Keses(string jaratSzam, int keses)
-        {
-
-        }
-        DateTime MikorIndul(string jaratSzam)
+        public DateTime MikorIndul(string jaratSzam)
         {
             return new DateTime();
         }
-        List<string> JaratokRepuloterrol(string repter)
+        public List<string> JaratokRepuloterrol(string repter)
         {
             return null;
         }
